e119ba3e3c6e927d42009d672bcdefe5c270ca12
DreamCoffee2 <70522204+DreamCoffee2@users.noreply.github.com>
2022-05-05 16:11:25 +0900

    파이썬 프로젝트로 변경
    파이썬 프로젝트로 변경

---
+++ Test.py
@@ -0,0 +1,13 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome('chromedriver')
+driver.get(url='https://www.google.com/')
