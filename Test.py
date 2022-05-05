02b08c90f01e1b8c9776b22b00e0e7377d56b022
DreamCoffee2 <70522204+DreamCoffee2@users.noreply.github.com>
2022-05-05 21:05:04 +0900

    오류 수정 & 데이터 저장 방식 변경
    오류 수정
    데이터 저장을 .csv파일로 저장함

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+import csv
 
 options = webdriver.ChromeOptions()
 
@@ -9,20 +11,25 @@ driver.implicitly_wait(5)
 
 driver.get(url='https://www.yogiyo.co.kr/mobile/#/414125/')
 
+driver.implicitly_wait(10)
+time.sleep(5)
 search_box = driver.find_element(by=By.CSS_SELECTOR, value='#content > div.restaurant-detail.row.ng-scope > '
                                                            'div.col-sm-8 > ul > li:nth-child(2) > a')
-driver.implicitly_wait(20)
 search_box.click()
 
+# 리뷰 데이터를 찾습니다.
 review = driver.find_elements(by=By.XPATH, value='//*[@id="review"]/li/p')
-
 
 for i in review:
     review_text = i.text
     print(review_text)
 
-f = open(r"C:\Users\User\Documents\GitHub\Web-crawler\새파일.txt", 'w', encoding="utf-8")
+a = 1
+f = open(r"C:\Users\User\Documents\GitHub\Web-crawler\데이터처리\reviewData.csv", 'w', encoding="utf-8-sig", newline='')
 for i in review:
-    f.write(i.text)
-    f.write("\n")
+    wr = csv.writer(f)
+    wr.writerow([a, i.text])
+    a += 1
+
 f.close()
+driver.quit()
