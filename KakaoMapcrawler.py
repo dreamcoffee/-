92055ce1cdffc55a92200ba6d99ba9e97ccb9e35
DreamCoffee2 <70522204+DreamCoffee2@users.noreply.github.com>
2022-05-06 19:35:43 +0900

    기능 추가
    [임시]
    - 가게 이름 수집
    - 리뷰 페이지 1~5페이지까지 넘기면서 리뷰 크롤링
    - 모인 데이터 .csv 파일 쓰기

--- KakaoMapcrawler.py
+++ KakaoMapcrawler.py
@@ -15,17 +15,19 @@ def main():
     driver.implicitly_wait(5)
     driver.get(url='https://map.kakao.com/')
     search(Enter)
-#    driver.quit()
+    driver.quit()
 
 
-def file():
+def file(review_data, name):
     f = open(r"C:\Users\User\Documents\GitHub\Web-crawler\데이터처리\reviewData2.csv", 'w', encoding="utf-8-sig", newline='')
     wr = csv.writer(f)
-    wr.writerow(["이름", "주소", "전화번호", "리뷰"])
-
+    wr.writerow(["이름", "리뷰"])
+    wr.writerows([name, review_data])
+    f.close()
 
 def search(Enter):
     global driver
+    name = []
     time.sleep(1)  # 추가적인 로딩 방지를 위해 추가적으로 1초 대기합니다.
 
     # 검색을 위한 구문입니다.
@@ -44,26 +46,37 @@ def search(Enter):
     view_details = driver.find_element(by=By.XPATH, value='//*[@id="info.search.place.list"]/li[1]/div[5]/div[4]/a[1]')
     view_details.send_keys(Keys.ENTER)
 
+    name.append(driver.find_element(by=By.CLASS_NAME, value='link_name').text)
+
     last_tab = driver.window_handles[-1]
+    first_tab = driver.window_handles[0]
     driver.switch_to.window(window_name=last_tab)
+
     driver.implicitly_wait(5)
-    time.sleep(1)
+    review_crawling(name)
+
+
+def review_crawling(name):
+    global driver
+    # 리뷰 데이터를 수집합니다.
+    review_data = []
+
     review_page = driver.find_elements(by=By.CLASS_NAME, value="link_page")
     review_page_num = len(review_page)
-    print(review_page_num)
 
+    for a in range(review_page_num):
+        next_page = driver.find_elements(by=By.CLASS_NAME, value='link_page')
+        next_page[a].click()
+        time.sleep(0.5)
+        review = driver.find_elements(by=By.CLASS_NAME, value='txt_comment')
+        for i in review:
+            review_data.append(i.text)
+    print("------결과------")
+    print(review_data)
+    print(name)
 
-#def review_crawling():
-#    global driver
-    # 리뷰 데이터를 수집합니다.
-#    review = driver.find_elements(by=By.CSS_SELECTOR, value='txt_comment > span')
-#    print(review)
+    file(review_data, name)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
